48cc70640f88300754b78feac4c372429bb84236
Sean Bhupathi <seanbhupathi@gmail.com>
2017-03-06 11:57:13 -0500

    navbar setup. master reducer ready to reduce.

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Navbar1 from "../containers/Navbar.js";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
+      <div>
+          <Navbar1 />
       </div>
     );
   }

ec67e46fbfa065d2398a614ff4977d68a8219d81
Sean Bhupathi <seanbhupathi@gmail.com>
2017-03-21 11:53:57 -0400

    ratings stars

--- front-end/src/containers/Ratings.js
+++ front-end/src/containers/Ratings.js
@@ -1,9 +1,13 @@
 import React, {Component} from "react";
+import Rating from "react-rating";
 
 class Ratings extends Component {
     render() {
         return(
-            <div></div>
+            <div className="text-center">
+                <Rating start={0} stop={10} fractions={10} empty={"glyphicon glyphicon-star-empty"} full={"glyphicon glyphicon-star"}/>
+            </div>
+
         )
     }
 }
